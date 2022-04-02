45a1d1899d3d36da1361ae01397b02a7140abd88
ryfazrin <riansera515@gmail.com>
2022-04-02 22:38:23 +0800

    change Book JsonResource

--- app/Http/Resources/Book.php
+++ app/Http/Resources/Book.php
@@ -14,11 +14,6 @@ class Book extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ];
+        return parent::toArray($request);
     }
 }
