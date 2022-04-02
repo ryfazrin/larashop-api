0fb8dd33e9f2f53ced46a76f4575d3e182244ecb
ryfazrin <riansera515@gmail.com>
2022-04-02 23:31:50 +0800

    added code field api_token

--- database/migrations/2022_04_02_152817_adds_api_token_to_users_table.php
+++ database/migrations/2022_04_02_152817_adds_api_token_to_users_table.php
@@ -14,7 +14,7 @@ class AddsApiTokenToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('api_token', 60)->unique()->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddsApiTokenToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['api_token']);
         });
     }
 }
